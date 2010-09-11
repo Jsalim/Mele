51f733f357e5d81bb4f18713c7abaa35922e0b41
Scott Leberknight <sleberkn@nearinfinity.com>
2010-09-11 11:28:01 -0400

    Make test use embedded Zookeeper server. Still needs work. See TODO in test.

--- src/test/java/com/nearinfinity/mele/store/hdfs/HdfsMeleTest.java
+++ src/test/java/com/nearinfinity/mele/store/hdfs/HdfsMeleTest.java
@@ -5,8 +5,6 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.UUID;
 
-import junit.framework.TestCase;
-
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
@@ -22,89 +20,119 @@ import org.apache.lucene.util.Version;
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.server.quorum.QuorumPeerMain;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
 import com.nearinfinity.mele.store.MeleConfiguration;
 import com.nearinfinity.mele.store.zookeeper.ZooKeeperFactory;
 
-public class HdfsMeleTest extends TestCase {
+public class HdfsMeleTest {
 
-	private FileSystem hdfsFileSystem;
+    private static File dataDirectory;
+    private FileSystem hdfsFileSystem;
 
-	@Override
-	protected void setUp() throws Exception {
-		ZooKeeperFactory.create(new MeleConfiguration(), new Watcher() {
-			@Override
-			public void process(WatchedEvent event) {
+    @BeforeClass
+    public static void setUpOnce() throws Exception {
+        dataDirectory = new File("target/zookeeper-data");
+        if (dataDirectory.exists()) {
+            rm(dataDirectory);
+        }
 
-			}
-		});
-		ZooKeeper zk = ZooKeeperFactory.getZooKeeper();
-		DeleteZkNode.delete(zk, "/mele");
-		rm(new File("./target/tmp"));
-		hdfsFileSystem = FileSystem.getLocal(new Configuration());
-	}
+        Thread zkDaemon = new Thread(new Runnable() {
 
-	public void testHdfsMele() throws Exception {
-		HdfsMele mele1 = getHdfsMele("./target/tmp/tmp1");
-		HdfsMele mele2 = getHdfsMele("./target/tmp/tmp2");
-		HdfsMele mele3 = getHdfsMele("./target/tmp/tmp3");
-		HdfsMele mele4 = getHdfsMele("./target/tmp/tmp4");
-		HdfsMele mele5 = getHdfsMele("./target/tmp/tmp5");
-		HdfsMele mele6 = getHdfsMele("./target/tmp/tmp6");
-		HdfsMele mele7 = getHdfsMele("./target/tmp/tmp7");
-		HdfsMele mele8 = getHdfsMele("./target/tmp/tmp8");
-		HdfsMele mele9 = getHdfsMele("./target/tmp/tmp9");
-		HdfsMele mele10 = getHdfsMele("./target/tmp/tmp10");
-		mele1.createDirectoryCluster("test");
-		populate(mele1, "test", "test-1");
-		populate(mele2, "test", "test-2");
-		populate(mele3, "test", "test-3");
-		populate(mele4, "test", "test-4");
-		populate(mele5, "test", "test-5");
-		populate(mele6, "test", "test-6");
-		populate(mele7, "test", "test-7");
-		populate(mele8, "test", "test-8");
-		populate(mele9, "test", "test-9");
-		populate(mele10, "test", "test-10");
-	}
+            @Override
+            public void run() {
+                QuorumPeerMain.main(new String[]{ "src/test/resources/zoo-embeddable.cfg" });
+            }
+        });
+        zkDaemon.setDaemon(true);
+        zkDaemon.start();
 
-	private void populate(HdfsMele mele, String cluster, String dir) throws Exception {
-		mele.createDirectory(cluster, dir);
-		Directory directory = mele.open(cluster, dir);
-		populate(directory,mele.getIndexDeletionPolicy(cluster, dir));
-	}
+        // TODO Need better way to wait for embedded server start than this sleep!
+        Thread.sleep(5000);
+    }
 
-	private void populate(Directory directory, IndexDeletionPolicy indexDeletionPolicy) throws Exception {
-		IndexWriter writer = new IndexWriter(directory, new StandardAnalyzer(Version.LUCENE_CURRENT), indexDeletionPolicy, MaxFieldLength.UNLIMITED);
-		for (int i = 0; i < 10000; i++) {
-			writer.addDocument(genDoc());
-		}
-		writer.close();
-	}
+    @Before
+    public void setUp() throws Exception {
+        ZooKeeperFactory.create(new MeleConfiguration(), new Watcher() {
+            @Override
+            public void process(WatchedEvent event) {
 
-	private Document genDoc() {
-		Document document = new Document();
-		document.add(new Field("id",UUID.randomUUID().toString(),Store.YES,Index.ANALYZED_NO_NORMS));
-		return document;
-	}
+            }
+        });
+        ZooKeeper zk = ZooKeeperFactory.getZooKeeper();
+        DeleteZkNode.delete(zk, "/mele");
+        rm(new File(dataDirectory, "tmp"));
+        hdfsFileSystem = FileSystem.getLocal(new Configuration());
+    }
 
-	private HdfsMele getHdfsMele(String dir) throws IOException {
-		File file = new File("./target/tmp/mele");
-		file.mkdirs();
-		MeleConfiguration conf = new MeleConfiguration();
-		conf.setBaseHdfsPath(file.getAbsolutePath());
-		conf.setHdfsFileSystem(hdfsFileSystem);
-		conf.setLocalReplicationPathList(Arrays.asList(dir));
-		conf.setUsingHdfs(true);
-		return new HdfsMele(conf);
-	}
+    @Test
+    public void testHdfsMele() throws Exception {
+        HdfsMele mele1 = getHdfsMele("tmp1");
+        HdfsMele mele2 = getHdfsMele("tmp2");
+        HdfsMele mele3 = getHdfsMele("tmp3");
+        HdfsMele mele4 = getHdfsMele("tmp4");
+        HdfsMele mele5 = getHdfsMele("tmp5");
+        HdfsMele mele6 = getHdfsMele("tmp6");
+        HdfsMele mele7 = getHdfsMele("tmp7");
+        HdfsMele mele8 = getHdfsMele("tmp8");
+        HdfsMele mele9 = getHdfsMele("tmp9");
+        HdfsMele mele10 = getHdfsMele("tmp10");
+        mele1.createDirectoryCluster("test");
+        populate(mele1, "test", "test-1");
+        populate(mele2, "test", "test-2");
+        populate(mele3, "test", "test-3");
+        populate(mele4, "test", "test-4");
+        populate(mele5, "test", "test-5");
+        populate(mele6, "test", "test-6");
+        populate(mele7, "test", "test-7");
+        populate(mele8, "test", "test-8");
+        populate(mele9, "test", "test-9");
+        populate(mele10, "test", "test-10");
+    }
 
-	private void rm(File file) {
-		if (file.isDirectory()) {
-			for (File f : file.listFiles()) {
-				rm(f);
-			}
-		}
-		file.delete();
-	}
+    private void populate(HdfsMele mele, String cluster, String dir) throws Exception {
+        mele.createDirectory(cluster, dir);
+        Directory directory = mele.open(cluster, dir);
+        populate(directory, mele.getIndexDeletionPolicy(cluster, dir));
+    }
+
+    private void populate(Directory directory, IndexDeletionPolicy indexDeletionPolicy) throws Exception {
+        IndexWriter writer =
+                new IndexWriter(directory, new StandardAnalyzer(Version.LUCENE_CURRENT), indexDeletionPolicy,
+                        MaxFieldLength.UNLIMITED);
+        for (int i = 0; i < 10000; i++) {
+            writer.addDocument(genDoc());
+        }
+        writer.close();
+    }
+
+    private Document genDoc() {
+        Document document = new Document();
+        document.add(new Field("id", UUID.randomUUID().toString(), Store.YES, Index.ANALYZED_NO_NORMS));
+        return document;
+    }
+
+    private HdfsMele getHdfsMele(String dir) throws IOException {
+        File file = new File(dataDirectory, "mele");
+        file.mkdirs();
+        MeleConfiguration conf = new MeleConfiguration();
+        conf.setBaseHdfsPath(file.getAbsolutePath());
+        conf.setHdfsFileSystem(hdfsFileSystem);
+        File fullDir = new File(dataDirectory, dir);
+        conf.setLocalReplicationPathList(Arrays.asList(fullDir.getPath()));
+        conf.setUsingHdfs(true);
+        return new HdfsMele(conf);
+    }
+
+    private static void rm(File file) {
+        if (file.isDirectory()) {
+            for (File f : file.listFiles()) {
+                rm(f);
+            }
+        }
+        file.delete();
+    }
 }
